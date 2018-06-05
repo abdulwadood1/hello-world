cbfa70a39c3cd2e533907981bdb11015573f5c64
abdulwadood1 <39953412+abdulwadood1@users.noreply.github.com>
2018-06-05 16:56:09 +0500

    Add files via upload
    A tic tac toe game made in VS in C# language. One of my first game

---
+++ WinFrmAppGame/WinFrmAppGame/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace WinFrmAppGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.R11 = new System.Windows.Forms.Button();
+            this.R12 = new System.Windows.Forms.Button();
+            this.R13 = new System.Windows.Forms.Button();
+            this.R21 = new System.Windows.Forms.Button();
+            this.R22 = new System.Windows.Forms.Button();
+            this.R23 = new System.Windows.Forms.Button();
+            this.R31 = new System.Windows.Forms.Button();
+            this.R32 = new System.Windows.Forms.Button();
+            this.R33 = new System.Windows.Forms.Button();
+            this.lbPlyr1 = new System.Windows.Forms.Label();
+            this.lbPlyr2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // R11
+            // 
+            this.R11.Location = new System.Drawing.Point(16, 88);
+            this.R11.Name = "R11";
+            this.R11.Size = new System.Drawing.Size(138, 65);
+            this.R11.TabIndex = 0;
+            this.R11.UseVisualStyleBackColor = true;
+            this.R11.Click += new System.EventHandler(this.R11_Click);
+            // 
+            // R12
+            // 
+            this.R12.Location = new System.Drawing.Point(160, 88);
+            this.R12.Name = "R12";
+            this.R12.Size = new System.Drawing.Size(138, 65);
+            this.R12.TabIndex = 0;
+            this.R12.UseVisualStyleBackColor = true;
+            this.R12.Click += new System.EventHandler(this.R11_Click);
+            // 
+            // R13
+            // 
+            this.R13.Location = new System.Drawing.Point(311, 88);
+            this.R13.Name = "R13";
+            this.R13.Size = new System.Drawing.Size(138, 65);
+            this.R13.TabIndex = 0;
+            this.R13.UseVisualStyleBackColor = true;
+            this.R13.Click += new System.EventHandler(this.R11_Click);
+            // 
+            // R21
+            // 
+            this.R21.Location = new System.Drawing.Point(16, 180);
+            this.R21.Name = "R21";
+            this.R21.Size = new System.Drawing.Size(138, 65);
+            this.R21.TabIndex = 0;
+            this.R21.UseVisualStyleBackColor = true;
+            this.R21.Click += new System.EventHandler(this.R11_Click);
+            // 
+            // R22
+            // 
+            this.R22.Location = new System.Drawing.Point(160, 180);
+            this.R22.Name = "R22";
+            this.R22.Size = new System.Drawing.Size(138, 65);
+            this.R22.TabIndex = 0;
+            this.R22.UseVisualStyleBackColor = true;
+            this.R22.Click += new System.EventHandler(this.R11_Click);
+            // 
+            // R23
+            // 
+            this.R23.Location = new System.Drawing.Point(311, 180);
+            this.R23.Name = "R23";
+            this.R23.Size = new System.Drawing.Size(138, 65);
+            this.R23.TabIndex = 0;
+            this.R23.UseVisualStyleBackColor = true;
+            this.R23.Click += new System.EventHandler(this.R11_Click);
+            // 
+            // R31
+            // 
+            this.R31.Location = new System.Drawing.Point(16, 272);
+            this.R31.Name = "R31";
+            this.R31.Size = new System.Drawing.Size(138, 65);
+            this.R31.TabIndex = 0;
+            this.R31.UseVisualStyleBackColor = true;
+            this.R31.Click += new System.EventHandler(this.R11_Click);
+            // 
+            // R32
+            // 
+            this.R32.Location = new System.Drawing.Point(160, 272);
+            this.R32.Name = "R32";
+            this.R32.Size = new System.Drawing.Size(138, 65);
+            this.R32.TabIndex = 0;
+            this.R32.UseVisualStyleBackColor = true;
+            this.R32.Click += new System.EventHandler(this.R11_Click);
+            // 
+            // R33
+            // 
+            this.R33.Location = new System.Drawing.Point(311, 272);
+            this.R33.Name = "R33";
+            this.R33.Size = new System.Drawing.Size(138, 65);
+            this.R33.TabIndex = 0;
+            this.R33.UseVisualStyleBackColor = true;
+            this.R33.Click += new System.EventHandler(this.R11_Click);
+            // 
+            // lbPlyr1
+            // 
+            this.lbPlyr1.AutoSize = true;
+            this.lbPlyr1.Location = new System.Drawing.Point(496, 88);
+            this.lbPlyr1.Name = "lbPlyr1";
+            this.lbPlyr1.Size = new System.Drawing.Size(0, 13);
+            this.lbPlyr1.TabIndex = 1;
+            // 
+            // lbPlyr2
+            // 
+            this.lbPlyr2.AutoSize = true;
+            this.lbPlyr2.Location = new System.Drawing.Point(496, 114);
+            this.lbPlyr2.Name = "lbPlyr2";
+            this.lbPlyr2.Size = new System.Drawing.Size(0, 13);
+            this.lbPlyr2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 445);
+            this.Controls.Add(this.lbPlyr2);
+            this.Controls.Add(this.lbPlyr1);
+            this.Controls.Add(this.R33);
+            this.Controls.Add(this.R32);
+            this.Controls.Add(this.R23);
+            this.Controls.Add(this.R22);
+            this.Controls.Add(this.R31);
+            this.Controls.Add(this.R13);
+            this.Controls.Add(this.R21);
+            this.Controls.Add(this.R12);
+            this.Controls.Add(this.R11);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button R11;
+        private System.Windows.Forms.Button R12;
+        private System.Windows.Forms.Button R13;
+        private System.Windows.Forms.Button R21;
+        private System.Windows.Forms.Button R22;
+        private System.Windows.Forms.Button R23;
+        private System.Windows.Forms.Button R31;
+        private System.Windows.Forms.Button R32;
+        private System.Windows.Forms.Button R33;
+        private System.Windows.Forms.Label lbPlyr1;
+        private System.Windows.Forms.Label lbPlyr2;
+    }
+}
+
---
+++ WinFrmAppGame/WinFrmAppGame/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFrmAppGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int plyr = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lbPlyr1.Text = "Player-1";
+        }
+
+        private void R11_Click(object sender, EventArgs e)
+        {
+            plyr++;
+
+            int rmd = plyr % 2;
+
+            Button btn = sender as Button;
+
+            if (rmd > 0)
+            {
+                //odd no need to display x symbole on 
+                btn.Text = "X";
+            }
+            else
+            { 
+                //even no need to display 0 symbole on button
+                btn.Text = "0";
+            }
+        }
+    }
+}
